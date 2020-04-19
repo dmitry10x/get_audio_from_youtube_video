b069ac7b3236e860ca167ea1640bb8b7cca6ad93
dmitry10x <fesfdi@gmail.com>
2020-04-19 18:16:57 +0300

    Wow! Tkinter

---
+++ README.md
@@ -0,0 +1,11 @@
+I watch youtube. And the majority of videos I just listen to. So, here is a little slapdash programm that takes url video, downloads it and converts to .Mp3.
+[Converter](http://prntscr.com/s2bcxl)
+
+
+How to make it work:
+
+INSTALL youtube-dl using pip
+
+Watch this video https://www.youtube.com/watch?v=ObuBysxtv9M and follow instructions
+
+
--- main.py
+++ main.py
@@ -1,9 +1,48 @@
-import pafy
+from __future__ import unicode_literals
+import youtube_dl
 
-url = 'https://www.youtube.com/watch?v=BqWPqZK-Ikg'
-video = pafy.new(url)
+from tkinter import *
 
-bestaudio = video.getbestaudio()
-bestaudio.download()
+def video_to_mp3(video_url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([str(video_url)])
 
+
+
+
+
+window = Tk()
+
+window.title("YouTube to MP3")
+
+window.geometry('350x200')
+
+lbl = Label(window, text="video link:")
+
+lbl.grid(column=0, row=0)
+
+txt = Entry(window,width=10)
+
+txt.grid(column=1, row=0)
+
+def clicked():
+    print(txt.get())
+    video_to_mp3(txt.get())
+    # res = "Welcome to " + txt.get()
+
+    lbl.configure(text='video link:')
+
+btn = Button(window, text="Make MP3", command=clicked)
+
+btn.grid(column=2, row=0)
+
+window.mainloop()
 
